2d44d4656e62913c218aa2522da48aff56c2e27e
sijab <kaczorowskid97@gmail.com>
2023-04-24 15:16:29 +0200

    feat(BACK-3): rename routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -15,6 +15,6 @@ userRouter.post("/register", register);
 userRouter.post("/login", login);
 userRouter.get("/confirm-account/:token", confirmAccount);
 userRouter.get("/:email", getUser);
-userRouter.post("/generate-reset", resetPassword);
-userRouter.post("/reset-password", setNewPassword);
+userRouter.post("/reset-password", resetPassword);
+userRouter.post("/set-new-password", setNewPassword);
 userRouter.patch("/update/:id", updateUser);
